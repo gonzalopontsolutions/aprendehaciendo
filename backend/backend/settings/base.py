6f05ea691b17342403b2780e5ddf475a2fe7b03b
Gonzalo Vitti <gonzalo4259@gmail.com>
2025-01-22 09:18:43 -0300

    changes in deploy.yml

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -135,6 +135,17 @@ CHANNEL_LAYERS = {
     },
 }
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB', 'postgres'),
+        'USER': os.environ.get('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
+        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
+    }
+}
+
 # Logging Configuration
 LOGGING = {
     "version": 1,
